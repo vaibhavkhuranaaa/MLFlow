5a00f0b34b23ab84f04c944d065199a4da1d604e
Vaibhav <vaibhavkhuranaaa@gmail.com>
2024-02-26 03:20:30 -0600

    params yaml added and model trainer added.

--- src/mlProject/components/model_trainer.py
+++ src/mlProject/components/model_trainer.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
-from mlProject import logger
+from src.mlProject import logger
 from sklearn.linear_model import ElasticNet
 import joblib
-from mlProject.entity.config_entity import ModelTrainerConfig
+from src.mlProject.entity.config_entity import ModelTrainerConfig
 
 
 
--- src/mlProject/pipeline/stage_04_model_trainer.py
+++ src/mlProject/pipeline/stage_04_model_trainer.py
@@ -1,6 +1,6 @@
-from mlProject.config.configuration import ConfigurationManager
-from mlProject.components.model_trainer import ModelTrainer
-from mlProject import logger
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.model_trainer import ModelTrainer
+from src.mlProject import logger
 
 
 
